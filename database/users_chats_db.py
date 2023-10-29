f70460265be52b341b29183fd90eb0e800af9b7c
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2023-10-29 05:49:05 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -108,7 +108,7 @@ class Database:
     
     async def get_settings(self, id):
         default = {
-            'button': SINGLE_BUTTON,
+            'text': SINGLE_BUTTON,
             'botpm': P_TTI_SHOW_OFF,
             'file_secure': PROTECT_CONTENT,
             'imdb': IMDB,
